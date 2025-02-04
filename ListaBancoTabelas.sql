4eb294b8edec0eb5818c05b0e38e25d804ce5552
RSulzbach <53040114+Raimundo-Sulzbach@users.noreply.github.com>
2025-02-04 06:59:00 -0300

    Add files via upload

---
+++ ListaBancoTabelas.sql
@@ -0,0 +1,20 @@
+-- Desafio - Dio X Heinekein X MySQL Workbench
+-- Criando e implementando um um Projeto Lógico de Banco de Dados para gerenciamento de OS - ordens de serviço em uma oficina mecânica.
+-- Autor: Raimundo Sulzbach
+--
+-- (*) Atenção! O script Apaga e reinicia o banco de dados dio_ecomercedb.
+--
+-- Data: 30/01/2025
+-- Ferramentas utilizadas: MySQL e PyCharm
+--
+-- Banco: dio_osdb - banco de dados para um sistema de gerenciamento de OS - ordens de serviço, em uma oficina mecânica.
+--
+
+-- Mostra bancos de dados existentes
+SHOW DATABASES;
+
+-- mostra tabelas do banco de dados
+SHOW TABLES;
+
+-- acessa o banco de dados dio_osdb - Ordens de serviço
+USE `dio_osdb` ;
